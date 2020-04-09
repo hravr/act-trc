b6f880696c795a39058f1050eb6bb29f388b5db8
hravr <agrinkiv74@gmail.com>
2020-04-09 23:35:14 +0300

    fm

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
-const App = () => {
+function App() {
   const url = 'http://localhost:4000/api/activitys';
-  const [activitys, setActivitys] = useState(null); //сховище
-  const [params, setParams] = useState({});
+  const [activitys, setActivitys] = useState(null);
+  const [items, setItems] = useState({});
   const form = useRef();
 
-  const fetchData = () => {
-    fetch(url) //мастХЕВ
+  // fetchData
+  function fetchData() {
+    fetch(url)
       .then(res => res.json()
-        .then(activitys => setActivitys(activitys)))
+        .then(activitys => setActivitys(activitys.reverse()))
+      )
   }
 
-  useEffect(() => { // запускає ф-цію один раз при пустому масиві
+  useEffect(() => {
     fetchData()
   }, [])
 
-  const createActivity = (params) => {
+  // createActivity
+  function createActivity(items) {
     form.current.reset();
     fetch(url, {
       method: 'POST',
-      body: JSON.stringify(params),
+      body: JSON.stringify(items),
       headers: {
         'Content-Type': 'application/json'
       }
@@ -31,16 +34,20 @@ const App = () => {
       .then(activitys => activitys);
   }
 
-  //сума часу
-  const time = ({ ftime, stime }) => {
-    const time = (new Date('2020-03-10T' + ftime)
+  //Sum Time
+  function sumTime({ ftime, stime }) {
+    const sumTime = (new Date('2020-03-10T' + ftime)
       - new Date('2020-03-10T' + stime)) / 60000;
-    return time >= 60
-      ? Math.trunc(time / 60) + ' hour ' + (time % 60 === 0 ? '' : time % 60 + ' minutes')
-      : time % 60 + ' minutes'
+    if (sumTime >= 60) {
+      return Math.trunc(sumTime / 60) + ' hour ' + (sumTime % 60 === 0 ? '' : sumTime % 60 + ' minutes')
+    }
+    else {
+      return sumTime % 60 + ' minutes'
+    }
   }
-  //швидкысть
-  const averageSpeed = ({ distance, ftime, stime }) => {
+
+  //Speed
+  function averageSpeed({ distance, ftime, stime }) {
     const average = distance / (
       (new Date('2020-03-10T' + ftime)
         - new Date('2020-03-10T' + stime)) / 60000)
@@ -48,26 +55,24 @@ const App = () => {
     return average.toFixed(1) + 'km/hour';
   }
 
-  //тотал
-  const total = (type) => {
+  //Total
+  function actTotal(type) {
     let sum = 0;
     activitys.map(activity => activity.act_type === type
       ? sum += activity.distance
       : null)
-    return sum
-      ;
+    return sum;
   }
 
-
-
-  //   // Удаление 
+  // Delete
   function deleteActivity(id) {
     fetch(url + '/' + id, {
       method: "DELETE"
-    }).then(setActivitys(activitys.map(act => act._id !== id))) // фільтрація джс
+    }).then(setActivitys(activitys.filter(act => act._id !== id)))
   }
 
-  const longest = (type) => {
+  // longest
+  function longest(type) {
 
     const typedActivities = activitys.filter(activity => activity.act_type === type
       ? activity.distance
@@ -81,55 +86,56 @@ const App = () => {
       return '0'
     }
     else {
-      const time = (new Date('2020-03-10T' + maxTypedActivitiesDistance.ftime)
+      const sumTime = (new Date('2020-03-10T' + maxTypedActivitiesDistance.ftime)
         - new Date('2020-03-10T' + maxTypedActivitiesDistance.stime)) / 60000;
-      const formatedTime = time >= 60
-        ? Math.trunc(time / 60) + 'h ' + (time % 60 === 0 ? '' : time % 60 + 'm')
-        : time % 60 + ' m'
-
-      return [maxTypedActivitiesDistance.date, ' ', formatedTime, ' ', maxTypedActivitiesDistance.distance, 'km'];
+      function formatedTime() {
+        if (sumTime >= 60) {
+          return Math.trunc(sumTime / 60) + 'h ' + (sumTime % 60 === 0 ? '' : sumTime % 60 + 'm')
+        } else {
+          return sumTime % 60 + ' m'
+        }
+      }
+      return [maxTypedActivitiesDistance.date, ' ', formatedTime(), ' ', maxTypedActivitiesDistance.distance, 'km'];
     }
   }
 
-
-
   return (
     activitys &&
-    <div>
+    <div className='app'>
       <p className='title'>Activity Tracker</p>
       <form className='form' ref={form} >
-        <input placeholder='Start Time' className={'params'} type='time'
-          onChange={(e) => setParams({ ...params, stime: e.target.value })} />
-        <input placeholder='Finish Time' className={'params'} type='time'
-          onChange={(e) => setParams({ ...params, ftime: e.target.value })} />
+        <input placeholder='Start Time' className={'items'} type='time'
+          onChange={(e) => setItems({ ...items, stime: e.target.value })} />
+        <input placeholder='Finish Time' className={'items'} type='time'
+          onChange={(e) => setItems({ ...items, ftime: e.target.value })} />
         <input
           placeholder='Distance (km)'
-          className={'params'}
+          className={'items'}
           type='number'
           min='0'
-          onChange={(e) => setParams({ ...params, distance: e.target.value })}
+          onChange={(e) => setItems({ ...items, distance: e.target.value })}
         />
-        <select className='params' onClick={(e) => setParams({ ...params, act_type: e.target.value })}>
+        <select className='items' onClick={(e) => setItems({ ...items, act_type: e.target.value })}>
           <option hidden>Choose type</option>
           <option>Runing</option>
           <option>Riding</option>
         </select>
-        <input className={'params'} type='button' value={'Save'} onClick={() => createActivity(params)} />
+        <input className={'items'} type='button' value={'Save'} onClick={() => createActivity(items)} />
       </form>
-      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+      <div className='flex'>
         <div className='mainBlock'>
           {activitys.map(activity =>
-            <div key={activity._id} className={'paramBlock'}>
-              <div className={'params'}>
+            <div key={activity._id} className={'itemBlock'}>
+              <div className={'items'}>
                 {activity.date}
               </div>
-              <div className={'params'}>
-                {time(activity)}
+              <div className={'items'}>
+                {sumTime(activity)}
               </div>
-              <div className={'params'}>
+              <div className={'items'}>
                 {activity.act_type}
               </div>
-              <div className={'params'}>
+              <div className={'items'}>
                 {activity.distance + 'km'}
               </div>
               <div>
@@ -140,9 +146,9 @@ const App = () => {
           )}
         </div>
         <div>
-          <div className='total'>
-            <p>Total run distance: {total('Runing') + 'km'}</p>
-            <p>Total ride distance: {total('Riding') + 'km'}</p>
+          <div className='actTotal'>
+            <p>Total run distance: {actTotal('Runing') + 'km'}</p>
+            <p>Total ride distance: {actTotal('Riding') + 'km'}</p>
           </div>
           <div className='longest'>
             <p>Longest run: </p>
